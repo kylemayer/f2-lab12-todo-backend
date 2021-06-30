6bf50296b23e7cac22e7023035bfe69eb9b9bc05
Kyle Mayer <x@kylemayer.dev>
2021-06-29 17:27:43 -0700

    working on put tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,10 +9,10 @@ const client = require('../lib/client');
 describe('app routes', () => {
   describe('routes', () => {
     let token;
-  
+
     beforeAll(async () => {
       execSync('npm run setup-db');
-  
+
       await client.connect();
       const signInData = await fakeRequest(app)
         .post('/auth/signup')
@@ -20,39 +20,81 @@ describe('app routes', () => {
           email: 'jon@user.com',
           password: '1234'
         });
-      
+
       token = signInData.body.token; // eslint-disable-line
     }, 10000);
-  
+
     afterAll(done => {
       return client.end(done);
     });
 
-    test('returns animals', async() => {
+    test('POST create todo list', async() => {
 
       const expectation = [
         {
-          'id': 1,
-          'name': 'bessie',
-          'cool_factor': 3,
-          'owner_id': 1
-        },
+          id: 2,
+          todo: 'brush teeth',
+          completed: false,
+          owner_id: 2
+        }
+      ];
+
+      await fakeRequest(app)
+        .post('/api/todos')
+        .send(expectation[0])
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      const data = await fakeRequest(app)
+        .get('/api/todos')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+
+    test('GET todo list', async() => {
+
+      const expectation = [
         {
-          'id': 2,
-          'name': 'jumpy',
-          'cool_factor': 4,
-          'owner_id': 1
-        },
-        {
-          'id': 3,
-          'name': 'spot',
-          'cool_factor': 10,
-          'owner_id': 1
+          id: 2,
+          todo: 'brush teeth',
+          completed: false,
+          owner_id: 2
         }
       ];
 
       const data = await fakeRequest(app)
-        .get('/animals')
+        .get('/api/todos')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+
+    test('PUT updates a todo item', async() => {
+
+      const expectation = [
+        {
+          id: 2,
+          todo: 'brush teeth',
+          completed: false,
+          owner_id: 2
+        }
+      ];
+
+      await fakeRequest(app)
+        .put('/api/todos/2')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      const data = await fakeRequest(app)
+        .get('/api/todos')
+        .set('Authorization', token)
         .expect('Content-Type', /json/)
         .expect(200);
 
